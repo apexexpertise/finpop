ca792dd4b3432b9243ad92c271ece2b75c9d26e1
Rayan lazaar <raya.lazaar@apex-expertise.com>
2015-05-15 10:41:03 +0100

    mis a jour Integration page "voir projet"

--- view/discover/view.html.php
+++ view/discover/view.html.php
@@ -1,38 +1,34 @@
 <?php
 /*
- *  Copyright (C) 2012 Platoniq y Fundación Fuentes Abiertas (see README for details)
- *	This file is part of Goteo.
+ * Copyright (C) 2012 Platoniq y Fundación Fuentes Abiertas (see README for details)
+ * This file is part of Goteo.
  *
- *  Goteo is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Goteo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  Goteo is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ * Goteo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with Goteo.  If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Goteo. If not, see <http://www.gnu.org/licenses/agpl.txt>.
  *
  */
-
-use Goteo\Core\View,
-    Goteo\Library\Text;
+use Goteo\Core\View, Goteo\Library\Text;
 
 // en la p�gina de cofinanciadores, paginaci�n de 20 en 20
 require_once 'library/pagination/pagination.php';
 
-$pagedResults = new \Paginated($this['list'], 9, isset($_GET['page']) ? $_GET['page'] : 1);
-
-
+$pagedResults = new \Paginated ( $this ['list'],9, isset ( $_GET ['page'] ) ? $_GET ['page'] : 1 );
 
 $bodyClass = 'discover';
 
 include 'view/prologue.html.php';
 
-include 'view/header.html.php' ?>
+include 'view/header.html.php'?>
 <link href="/view/css/custom.css" rel="stylesheet" type="text/css">
 
 
@@ -59,77 +55,95 @@ $(document).ready(function(){
 
 <div class="jumbotron" id="resultat_projet">
 
-	<div class="container"> 
+	<div class="container">
 		<div class="content-center2">
-			<span id="tit-slid-resultat"> les projets&nbsp;</span> <span id="type"><?php echo $_GET['type'];?> </span> 
-			
-			<p id="content-slide2">
-				Par Cat&eacute;gorie, Lieu ou Type de r&eacute;compenses et
-				Retomb&eacute;es,<br> <span id="content-slide3">Localisez le projet
-				</span><span id="content-slide4">qui vous motive le plus</span>
+			<span id="tit-slid-resultat"> les projets&nbsp;</span> <span
+				id="type"><?php echo $_GET['type'];?> </span>
 
-			</p>
+
+			<div id="retour-boutton">
+				<a class="" href="/discover">
+					<button type="submit" class="btn btn-primary retour-btn"
+						name="retour">
+						<i class="fa fa-chevron-left"></i>&nbsp;retour aux resultat
+					</button>
+				</a>
+			</div>
+
+
+
+
 		</div>
 	</div>
+
 </div>
 
 
-       <!--   <div id="sub-header">
+<!--   <div id="sub-header">
             <div>
                 <h2 class="title"><?php echo $this['title']; ?></h2>
             </div>
 
         </div>-->
-        
-        
+
+
 <div class="container" id="projects-area">
 	<div class="row clearfix">
-	
-	<div class="row clearfix">
+
+		<div class="row clearfix">
 			<div class="col-md-3 column">
 			
 			   <?php
-												
-												echo new View ( 
+						
+						echo new View ( 
 
-												'view/discover/searcher.html.php', array (
-														'categories' => 
+						'view/discover/searcher.html.php', array (
+								'categories' => 
 
-														$categories,
-														'locations' => $locations,
-														'rewards' => $rewards 
-												) );
-												?>
+								$categories,
+								'locations' => $locations,
+								'rewards' => $rewards 
+						) );
+						?>
 				</div>
-				
-				<div class="col-md-9 column box-border-left">
+
+			<div class="col-md-9 column box-border-left">
 				<a href="#" class="go_top">Remonter</a>
-				
-				
-				
-					<div class="widget projects">
-					<h2 class="title-voirproj"><?php echo $list['title']?>
+
+
+
+				<div class="widget projects">
+					<h2 class="title-voirproj">Tous les projets
 
 					</h2>
 
 					<hr id="ligne-voirproj">
 				
-                <?php while ($project = $pagedResults->fetchPagedRow()) :
-                		echo '<div class="col-md-9 column box-border-left">';
-                        echo new View('view/project/widget/project.html.php', array(
-                            'project' => $project
-                            ));
-                        echo '</div>';
-                endwhile; ?>
+                <?php
+																
+																while ( $project = $pagedResults->fetchPagedRow () ) :
+																	/*echo '<div class="col-md-9 column box-border-left">';*/
+																	echo new View ( 'view/project/widget/project.html.php', array (
+																			'project' => $project 
+																	) );
+																	/*echo '</div>';*/
+																endwhile
+																;
+																?>
+																
+																</div>
             <ul id="pagination">
-                <?php   $pagedResults->setLayout(new DoubleBarLayout());
-                        echo $pagedResults->fetchPagedNavigation(); ?>
+                <?php
+																
+																$pagedResults->setLayout ( new DoubleBarLayout () );
+																echo $pagedResults->fetchPagedNavigation ();
+																?>
             </ul>
-    </div>
-</div>       
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-</div>
-        <?php include 'view/footer.html.php' ?>
+<?php include 'view/footer.html.php'?>
     
 <?php include 'view/epilogue.html.php' ?>
